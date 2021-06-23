60693e42dfcb97aea0e0baee055268c670d5922a
Camsz27 <camiloagr2707@gmail.com>
2021-06-23 17:10:44 -0500

    Number of books changes appropiately in the order container

--- script.js
+++ script.js
@@ -1,4 +1,9 @@
+const bookContainer = document.querySelector("#bookContainer");
+const orderContainer = document.querySelector("#orderContainer");
+
 let myLibrary = [];
+let bookNumber = 0;
+let booksRead = 0;
 
 function Book(title, author, pageNum, read) {
   this.title = title;
@@ -14,6 +19,19 @@ function addBookToLibrary(book) {
   myLibrary.push(book);
 }
 
+function numberOfBooks() {
+  bookNumber = myLibrary.length;
+}
+
+function changeOrderContainer() {
+  const numberOfBooks = document.querySelector("#numberOfBooks");
+  const booksReadText = document.querySelector("#booksRead");
+  const booksNotRead = document.querySelector("#booksNotRead");
+  numberOfBooks.textContent = `Total number of books: ${bookNumber}`;
+  booksReadText.textContent = `Read: ${booksRead}`;
+  booksNotRead.textContent = `Left to Read: ${bookNumber - booksRead}`;
+}
+
 const hobbit = new Book("The Hobbit", "J.R.R. Tolkien", 295, "not read yet");
 const book2 = new Book("Book 2", "Author 1", 495, "read");
 const book3 = new Book("Book 3", "Author 2", 395, "not read yet");
@@ -26,4 +44,8 @@ addBookToLibrary(book3);
 addBookToLibrary(book4);
 addBookToLibrary(book5);
 
+numberOfBooks();
+changeOrderContainer();
+
 console.log(myLibrary);
+console.log(numberOfBooks());
